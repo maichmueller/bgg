68f44e80b3f6f110f91c09db2adf688999c860d4
Michael A <m.aichmueller@gmail.com>
2020-01-12 17:22:03 +0100

    remove tqdm.cpp as submodule again, since it doesnt work

---
+++ deps/tqdm/include/tqdm/tqdm_register.h
@@ -0,0 +1,16 @@
+//
+// Created by Michael on 12.01.20.
+//
+
+#ifndef ALPHAZERO_TQDM_REGISTER_H
+#define ALPHAZERO_TQDM_REGISTER_H
+
+
+
+class tqdm_register {
+
+};
+
+
+
+#endif //ALPHAZERO_TQDM_REGISTER_H
---
+++ deps/tqdm/src/tqdm_register.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michael on 12.01.20.
+//
+
+#include "tqdm_register.h"
