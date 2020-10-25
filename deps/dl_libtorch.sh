38c2d5f7b92a9626cce6c243053dad44b9271b7d
der4twu <m.aichmueller@gmail.com>
2020-10-25 11:57:21 +0100

    workflow test

--- deps/dl_libtorch.sh
+++ deps/dl_libtorch.sh
@@ -1,14 +1,70 @@
-#!/bin/bash -x
+#!/bin/bash
 
-base_url=https://download.pytorch.org/libtorch/cu102
+# do !/bin/bash -x to see all commands and variables expanded as they are called (-v for not expanded variables)
+
+cuda=false
+os=linux
+build=debug  # by default we would grab the debug package of libtorch (windows only)
+for i in "$@"
+do
+case $i in
+    -c=*|--cuda=*)
+    cuda="${i#*=}"
+    shift # past argument=value
+    ;;
+    -s=*|--os=*)
+    os="${i#*=}"
+    shift # past argument=value
+    ;;
+    -b=*|--build=*)
+    build="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+base_url=https://download.pytorch.org/libtorch/cpu
+if [ "$cuda" = true ]; then \
+  base_url=https://download.pytorch.org/libtorch/cu102
+fi
+
 version=1.6.0
-filename=libtorch-cxx11-abi-shared-with-deps-$version.zip
-torch_url=$base_url/$filename
-target_dir=$(dirname $(readlink -f $0))
+
+if [ "$os" == "lin" ] || [ "$os" == "linux" ] || [ "$os" == "ubuntu" ]; then \
+  filename=libtorch-cxx11-abi-shared-with-deps-$version.zip; \
+
+elif [ "$os" == "mac" ] || [ "$os" == "apple" ] || [ "$os" == "osx" ]; then \
+  if [ "$cuda" = true ]; then \
+    echo "CUDA setting '$cuda' and operating system '$os' are incompatible. Stopping.";
+  fi
+
+  filename=libtorch-macos-$version.zip; \
+
+elif [ "$os" == "win" ] || [ "$os" == "windows" ] || [ "$os" == "windows10" ]; then \
+  if [ "$build" == "debug" ]; then \
+    filename=libtorch-win-shared-with-deps-debug-$version%2Bcpu.zip; \
+  elif [ "$build" == "release" ]; then \
+    filename=libtorch-win-shared-with-deps-$version%2Bcpu.zip; \
+  else
+    echo "Build type $build not found. Stopping."
+  fi
+
+else
+  echo "Operating system $os not found. Stopping."
+fi
+
+url=$base_url/$filename
+target_dir=$(dirname $(readlink -f $0))  # target dir is the dir of this script
+
+printf "%s\n" "Will attempt to download:" "OS: $os" "CUDA: $cuda" "BUILD: $build" "URL: $url" "TARGET DIR: $target_dir"
+
 if [ ! -d "$target_dir"/libtorch ]; then \
-  wget --directory-prefix="$target_dir" $torch_url; \
+  wget --directory-prefix="$target_dir" $url; \
   unzip "$target_dir"/$filename -d "$target_dir"; \
   rm "$target_dir"/$filename; \
 else
-  echo "folder 'libtorch' already exists -> exiting."; \
+  echo "Folder 'libtorch' already exists. Stopping."; \
 fi
